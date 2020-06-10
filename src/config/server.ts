c13c92e9e61d928318ea2de1c32f151fb80cf0f9
CAMIRO <juancamilosc@hotmail.com>
2020-06-10 13:21:06 +0800

    adding graphQL

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,9 +1,25 @@
 import express from 'express';
-    const app = express()
-    const PORT : string|number = process.env.PORT || 5000;
 
-    app.use("*",(req, res) =>{
-        res.send("<h1>Welcome to your simple server! Awesome right</h1>");
-    });
 
-    app.listen(PORT,() => console.log(`hosting @${PORT}`));
+const app = express();
+const graphqlHTTP = require('express-graphql');
+const schema = require('../graphQL/schema');
+
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    }),
+);
+  
+app.listen(4000);
+
+app.use("*",(req, res) =>{
+    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+});
+
+const PORT : string|number = process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log(`Server started on port ${PORT}`));
