92bf25cbf20cf9fd0995c76fdb94aa1bff0aff2b
CAMIRO <juancamilosc@hotmail.com>
2020-06-10 13:50:37 +0800

    adding react

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 const app = express();
 const graphqlHTTP = require('express-graphql');
 const schema = require('../graphQL/schema');
+const path = require('path')
 
 
 app.use(
@@ -14,11 +15,20 @@ app.use(
     }),
 );
   
-app.listen(4000);
+// app.listen(4000);
 
-app.use("*",(req, res) =>{
-    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+// app.use("*",(req, res) =>{
+//     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+// });
+
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+
+app.use(express.static('public'));
 
 const PORT : string|number = process.env.PORT || 5000;
 
