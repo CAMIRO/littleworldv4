c24023148a983791ace8f2cbc44a5e13754b43d4
CAMIRO <juancamilosc@hotmail.com>
2020-06-11 16:25:05 +0800

    formating files with lint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import {Button } from 'react-bootstrap'
-import styled from "styled-components"
+import { Button } from 'react-bootstrap';
+import styled from 'styled-components';
 import { AppNavBar } from './components/appNavBar';
-
-
 
 const App: React.FC = () => {
     return (
         <Wrapper>
-           <AppNavBar/>
+            <AppNavBar />
         </Wrapper>
-      
-
     );
-}
+};
 const Wrapper = styled.div`
-  display: flex;
-  overflow: hidden;
-  width: 100%;
-  min-height: 100%;
-  flex-direction: row;
-  position: absolute;
+    display: flex;
+    overflow: hidden;
+    width: 100%;
+    min-height: 100%;
+    flex-direction: row;
+    position: absolute;
 `;
 
 export default App;
