d3c1428973c41cd1a30b64680866919424b7877d
CAMIRO <juancamilosc@hotmail.com>
2020-06-17 10:23:35 +0800

    runnig build

---
+++ public/build/precache-manifest.e2458675a7c0324d074c229cdd66dd46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3171375f69e7981b8eaf395fbce5b42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7974a4759190835b20f4",
+    "url": "/static/css/main.8b153131.chunk.css"
+  },
+  {
+    "revision": "1c2cbe7c89f82d1789b8",
+    "url": "/static/js/2.bea99292.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bea99292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7974a4759190835b20f4",
+    "url": "/static/js/main.9584bb27.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
