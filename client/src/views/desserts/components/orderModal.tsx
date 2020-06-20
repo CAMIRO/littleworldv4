ba7b242b3aa4653a1b9fa53bf7c5a63184dfbd63
CAMIRO <juancamilosc@hotmail.com>
2020-06-20 18:30:54 +0800

    first steps Formik

--- client/src/views/desserts/components/orderModal.tsx
+++ client/src/views/desserts/components/orderModal.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Button, Modal } from 'react-bootstrap';
+// styles
+import { Button, Modal, Form, Row, Col } from 'react-bootstrap';
 import { Check } from 'react-feather';
-
+// formik
+import { Formik } from 'formik';
 interface OrderModalProps {
     showFormModal: boolean;
     handleShowFormModal: () => void;
@@ -12,9 +14,31 @@ export const OrderModal: React.FC<OrderModalProps>  = ({ showFormModal, handleSh
     return (
         <Modal size="lg" show={showFormModal} onHide={handleShowFormModal}>
                 <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>Make your order now</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <Formik initialValues={{ email: 'yolo@yolo' }} onSubmit={(data) => {
+                            console.log(data)
+                    }}>{({ values, handleChange, handleBlur, handleSubmit })=>(
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group as={Row} controlId="formPlaintextEmail">
+                                <Form.Label column sm="2">
+                                    Email
+                                </Form.Label>
+                                <Col sm="10">
+                                    <Form.Control 
+                                        name="email"
+                                        value={values.email} 
+                                        onChange={handleChange} 
+                                         
+                                
+                                        defaultValue="email@example.com" 
+                                    />
+                                </Col>
+                            </Form.Group>
+                        </Form>
+                    )}</Formik>
+                </Modal.Body>
                 <Modal.Footer>
                 <Button block size="lg" variant="primary" onClick={handleShowFormModal}>
                 <Check color="#FFF" size={23} /> SUBMIT ORDER
